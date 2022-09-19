104e0087355c803fc8a1b088a420414d80bc48d6
iwa0208 <iwacchi28@gmail.com>
2022-09-19 15:52:14 +0900

    完成

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,22 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList)
   return await result
 }
 
-function test() {
-  return
+function test(userList) {
+  return new Promise(function(resolve){
+    setTimeout(function(){resolve(userList.map(buildFullName))}, 3000)
+  })
 }
 
+function buildFullName(data) {
+  // Station14の問題はこちらです。想定する答えをここを書いてください。
+  // 氏名がわかるようにしつつ、半角スペースで繋いでください。
+  // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
+  let first = data.first_name
+  let family = data.family_name
+  data.full_name = family + ' ' + first
+  return data
+}
